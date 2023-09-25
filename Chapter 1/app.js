c6e6dbb6901e2e881bb6a7dab9908198e09ef5ba
Abdul Wasay Sarwar <136677108+Wasaysarwar@users.noreply.github.com>
2023-09-26 04:38:43 +0500

    Add files via upload

---
+++ Chapter 1/app.js
@@ -0,0 +1,47 @@
+//Write a script to greet your website visitor using JS alert box
+
+// alert("Assalamualikum");
+
+//. Write a script to display following message on your web page 
+
+// alert("Error! please enter a valid password")
+
+//Write a script to display following message on your web page
+
+// alert("Welcome to Js land...\nHappy coding!")
+
+//Write a script to display following messages in sequence
+
+// alert("Js Land...")
+
+//Declare a variable called username.
+
+// var username;
+
+//Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// var myName = 'wasaysarwar';
+// document.write(myName)
+
+// Write script to
+
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var message = "hello world";
+// alert(message);
+
+//Write a script to display the following alert using one JS variable:
+
+// alert('PIZZA\nPIZZ\nPIZ\nPI\nP')
+
+//Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var email = "my email address is example@example.com "
+// alert (email + "\n" )
+ 
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
