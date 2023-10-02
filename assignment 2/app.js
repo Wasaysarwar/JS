6f2c8fe72329410ffb0e6089ef8fa27839c83081
Abdul Wasay Sarwar <136677108+Wasaysarwar@users.noreply.github.com>
2023-10-03 00:04:09 +0500

    Add files via upload

---
+++ assignment 2/app.js
@@ -0,0 +1,31 @@
+var a = +prompt("Enter First Value");
+var b = +prompt("Enter second Value");
+var add = a + b;
+
+var multiply = a*b
+
+document.write(
+  "<table border='3px'>" +
+    "<tr>" +
+    "<th>" +
+    "ADDITION" +
+    "</th>" +
+    "</tr>" +
+    "<tr>" +
+    "<td>" +
+    add +
+    "</td>" +
+    "</tr>" +
+    "<tr>" +
+    "<th>" +
+    "MULTIPICATION" +
+    "</th>" +
+    "</tr>" +
+
+    "<tr>"+
+    "<td>" +
+    multiply +
+    "</td>" +
+    "</tr>"+
+    "</table>"
+);
