ce7214a3cf1bfc87a6895fa104dfbb8f60bc29b2
Abdul Wasay Sarwar <136677108+Wasaysarwar@users.noreply.github.com>
2023-10-06 00:03:13 +0500

    Add files via upload

---
+++ assignment 3/app.js
@@ -0,0 +1,97 @@
+//3. Write a program to print numeric counting from 1 to 10.
+
+
+// for(var i = 1; i <= 10;i++){
+//     document.write(i+"<br>");
+// }
+
+
+// 4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+//.,,..,,
+
+//5. Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+// var arr = ["apple", "banana" , "mango" , "Orange" , "pineapple" , "kiwi"];
+
+// for(var i = 0 ;i < arr.length ;i++){
+///     document.write(arr[i]+"<br>");
+//     document.write("Element at index "+i+ arr[i] +"<br>");
+    
+// }
+
+//6. Generate the following series in your browser. See
+// example output.
+
+// for(var i = 0; i <= 15 ;i++){
+//     document.write(i+"<br>");
+// }
+// for(var i = 10; i >= 1; i--){
+//     document.write(i+"<br>")
+// }
+
+// for(var i = 1; i < 20; i+=2){
+//     document.write(i+"<br>")
+// }
+
+// for(var i = 2; i < 20; i+=2){
+//     document.write(i+"<br>")
+// }
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(i * 2 * 1000 +"<br>")
+// }
+
+// var arr = ["cake" , "apple pie" , "Cookies" , "Chips" ,"patties"];
+// var check = prompt("Enter What Item You Need");
+// var flag = false;
+
+// for (var i=0; i < arr.length; i++) {
+    
+//     if(check === arr[i]){
+//         flag = true;
+//         break;
+//     }
+// }
+// if(flag==true){
+//     document.write(check +" is available at index" +" "+ +i+ " in our bakery"+"<br>");
+// }
+// else{
+//     document.write('Sorry, We do not have this item');
+// }
+
+
+//8. Write a program to identify the largest number and smallest in the
+// given array.
+
+// var arr = [10, 45, 4 , 91];
+// var largest = arr[0]
+
+// for(var i = 1; i< arr.length;i++){
+//     if(arr[i]>largest){
+//        largest = arr[i]
+//     }
+// }
+// document.write(largest+"<br>")
+
+
+// var arr = [10, 45, 4 , 91];
+// var smallest = arr[0]
+
+// for(var i = 1; i< arr.length;i++){
+//     if(arr[i]>smallest){
+//        largest = arr[i]
+//     }
+// }
+// document.write(smallest)
+
+
+// for(var i = 0;i < 20;i++){
+//     document.write(i*5+5+"<br>")
+
+// }
